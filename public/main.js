19e213d54eca34cd54ff06c0a24d1587e276428b
loodnn <loudjane@163.com>
2021-03-03 12:33:53 +0800

    init

---
+++ public/main.js
@@ -0,0 +1,97 @@
+getHTML.onclick = () => {
+  const request = new XMLHttpRequest();
+  request.open("GET", "/3.html");
+  request.onload = () => {
+    const div = document.createElement("div");
+    div.innerHTML = request.response;
+    document.body.appendChild(div);
+  };
+  request.onerror = () => {};
+  request.send();
+};
+
+getJS.onclick = () => {
+  const request = new XMLHttpRequest();
+  request.open("GET", "/2.js");
+  request.onload = () => {
+    //创建script标签
+    const script = document.createElement("script");
+    //填写script内容
+    script.innerHTML = request.response;
+    //加到正文中
+    document.body.appendChild(script);
+  };
+  request.onerror = () => {};
+  request.send();
+};
+
+getCSS.onclick = () => {
+  const request = new XMLHttpRequest();
+  request.open("GET", "/style.css");
+  request.onreadystatechange = () => {
+    console.log(request.readyState);
+    if (request.readyState === 4) {
+      if (request.status >= 200 && request.status < 300) {
+        const style = document.createElement("style");
+        //填写style内容
+        style.innerHTML = request.response;
+        //加到正文中
+        document.head.appendChild(style);
+      } else {
+        alert("加载CSS失败");
+      }
+    }
+  };
+  request.send();
+};
+
+getXML.onclick = () => {
+  const request = new XMLHttpRequest();
+  request.open("GET", "/4.xml");
+  request.onreadystatechange = () => {
+    if (request.readyState === 4) {
+      if (request.status >= 200 && request.status < 300) {
+        const dom = request.responseXML;
+        const text = dom.getElementsByTagName("warning")[0].textContent;
+        console.log(text.trim());
+      }
+    }
+  };
+  request.send();
+};
+
+getJSON.onclick = () => {
+  const request = new XMLHttpRequest();
+  request.open("GET", "/5.json");
+  request.onreadystatechange = () => {
+    if (request.readyState === 4) {
+      if (request.status >= 200 && request.status < 300) {
+        const object = JSON.parse(request.response);
+        myName.textContent = object.name;
+      }
+    }
+  };
+  request.send();
+};
+
+let n = 2;
+getPage.onclick = () => {
+  const request = new XMLHttpRequest();
+  request.open("GET", `/page${n}`);
+  request.onreadystatechange = () => {
+    if (request.readyState === 4) {
+      if (request.status >= 200 && request.status < 300) {
+        console.log(request.response);
+        const array = JSON.parse(request.response);
+        array.forEach((item) => {
+          const li = document.createElement("li");
+          li.textContent = item.id;
+          console.log(xxx);
+          xxx.appendChild(li);
+        });
+      }
+    }
+    n += 1;
+  };
+  request.send();
+};
